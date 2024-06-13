9035797c0e77ad874b67d02fd2505de59a5d936a
morzdz <manon.drozdz@gmail.com>
2024-06-13 14:21:39 +0200

    creation du crud avec les routes (GET, POST, PUT, PATCH, DELETE) sur http://localhost:3000/users

--- app.js
+++ app.js
@@ -1,8 +1,82 @@
 const express = require("express");
-const port = process.env.PORT || 5000;
+const bodyParser = require("body-parser");
+const port = 3000;
 
 const app = express();
 
+app.use(bodyParser.json());
+
+let users = [
+    { id: 1, name: 'Alice', email: 'alice@example.com' },
+    { id: 2, name: 'Bob', email: 'bob@example.com' },
+    { id: 3, name: 'Charlie', email: 'charlie@example.com' }
+];
+
+app.get("", (req, res) => {
+    res.status(200).send("hello world")
+})
+
+app.get("/users", (req, res) => {
+    res.status(200).send(users)
+})
+
+app.get("/users/:id", (req, res) => {
+    const userId = parseInt(req.params.id, 10);
+    const user = users.find(u => u.id === userId);
+
+    if (user) {
+        res.status(200).send(user);
+    } else {
+        res.status(404).send({ message: 'Utlisateur non trouvé'});
+    }
+})
+
+app.post("/users", (req, res) => {
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name,
+        email: req.body.email
+    };
+    users.push(newUser);
+    res.status(201).send(newUser);
+})
+
+app.put("/users/:id", (req, res) => {
+    const userId = parseInt(req.params.id, 10);
+    const userIndex = users.findIndex((u) => u.id === userId);
+
+    if (userIndex !== -1) {
+        users[userIndex] = { ...users[userIndex], ...req.body };
+        res.status(200).send(users[userIndex]);
+    } else {
+        res.status(404).send({ message: 'Utlisateur non trouvé'});
+    }
+});
+
+app.patch("/users/:id", (req, res) => {
+    const userId = parseInt(req.params.id, 10);
+    const userIndex = users.findIndex((u) => u.id === userId);
+
+    if (userIndex !== -1) {
+        users[userIndex] = { ...users[userIndex], ...req.body };
+        res.status(200).send(users[userIndex]);
+    } else {
+        res.status(404).send({ message: 'Utlisateur non trouvé'});
+    }
+});
+
+app.delete("/users/:id", (req, res) => {
+    const userId = parseInt(req.params.id, 10);
+    const userIndex = users.findIndex((u) => u.id === userId);
+
+    if (userIndex !== -1) {
+        users.splice(userIndex, 1);
+        res.status(200).send();
+    } else {
+        res.status(404).send({ message: 'Utlisateur non trouvé'});
+    }
+})
+
 app.listen(port, () => {
-    console.log('Server listening on 5000');
-})  
+    console.log(`Serveur démarré sur http://localhost:${port}`);
+}); 
